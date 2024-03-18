5afd521c8a6c3d875ed7835d174310e0805fe859
AxelRN <robertnaudinaxel@gmail.com>
2024-03-18 12:32:33 -0400

    file extension

--- src/app/Ressources/Document.php
+++ src/app/Ressources/Document.php
@@ -10,6 +10,7 @@ class Document extends Ressource
     public function create(int $key, string $type, string $libelle, string $file) :array
     {
         // Ouvrir le fichier en lecture
+        $extension = pathinfo($file, PATHINFO_EXTENSION);
         $fileHandle = fopen($file, 'r');
         // Créer un flux multipart contenant le fichier
         $multipartStream = new MultipartStream([
@@ -27,7 +28,11 @@ class Document extends Ressource
             ],
             [
                 'name' => 'File',
-                'contents' => $fileHandle
+                'contents' => $fileHandle,
+                'filename' => basename($file) . '.' . $extension,
+                'headers' => [
+                    'Content-Type' => mime_content_type($file)
+                ]
             ]
         ]);
 
